1fcd230f60ba8fe0523bd03cd3173c9c661d58a3
taruna-28 <126285576+taruna-28@users.noreply.github.com>
2024-05-31 19:45:52 +0530

    Create inputSum.java
    Sum of two number

---
+++ inputSum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class inputSum {
+    public static void main(String[] args) {
+
+        Scanner tc = new Scanner(System.in);
+        System.out.print("Enter a:");
+        int a = tc.nextInt();
+        System.out.print("Enter b:");
+        int b = tc.nextInt();
+
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+
+}
